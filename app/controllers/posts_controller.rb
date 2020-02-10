4ec6715bd0ef12929e566b68677739c50e338332
Shunsuke <shun.sskd05@icloud.com>
2020-02-10 05:05:09 +0000

    [Change] posts/index 部分テンプレート化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    posts = Post.all
   end
 
   def new
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to posts_path
+      redirect_to root_path
     else
       redirect_to new_post_path
     end
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     post.update(post_params)
-    redirect_to posts_path
+    redirect_to root_path
   end
 
   def show
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_path
+    redirect_to root_path
   end
 
   private
