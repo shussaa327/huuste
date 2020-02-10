4505983be06e5f214d78981886a7e66f5286d430
Shunsuke <shun.sskd05@icloud.com>
2020-02-10 06:17:18 +0000

    [Add] users/show,destroy機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -13,6 +14,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
   end
 
   private
