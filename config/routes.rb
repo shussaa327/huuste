e479ae765928dd1b3c1fc1aaae6c12a4ea94ed58
Shunsuke <shun.sskd05@icloud.com>
2020-02-12 16:04:24 +0000

    [Add] follower,following-page表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ root "homes#top"
 get "about" => "homes#about"
 post "follow/:id" => "relationships#follow", as: "follow"
 post "unfollow/:id" => "relationships#unfollow", as: "unfollow"
+get "following/:id" => "users#following"
+get "follower/:id" => "users#follower"
 devise_for :users
 resources :shops do
   resources :posts
