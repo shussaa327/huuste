d15db511706e066352d03df81fea3313b5f0ec7d
Shunsuke <shun.sskd05@icloud.com>
2020-02-26 14:23:15 +0000

    [Add] PostCommentモデル

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :user_recommendations, dependent: :destroy
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   belongs_to :user, optional: true
   belongs_to :shop, optional: true
   attachment :image
