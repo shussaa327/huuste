dab27cc53857ddd516b6be254a20583b371e7f26
Shunsuke <shun.sskd05@icloud.com>
2020-02-27 15:35:52 +0000

    [Add] お気に入り機能Ajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_141520) do
+ActiveRecord::Schema.define(version: 2020_02_27_145956) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
