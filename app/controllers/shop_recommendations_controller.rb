6bd1131c309fa0999ea2406e15bb0e705d42ad0a
Shunsuke <shun.sskd05@icloud.com>
2020-02-28 08:49:24 +0000

    [Update] shop_recommend機能Ajax化

--- app/controllers/shop_recommendations_controller.rb
+++ app/controllers/shop_recommendations_controller.rb
@@ -4,16 +4,14 @@ class ShopRecommendationsController < ApplicationController
   end
 
   def create
-    shop = Shop.find(params[:shop_id])
-    shop_recommendation = current_user.shop_recommendations.new(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    shop_recommendation = current_user.shop_recommendations.new(shop_id: @shop.id)
     shop_recommendation.save
-    redirect_to shop_path(shop)
   end
 
   def destroy
-    shop = Shop.find(params[:shop_id])
-    shop_recommendation = current_user.shop_recommendations.find_by(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    shop_recommendation = current_user.shop_recommendations.find_by(shop_id: @shop.id)
     shop_recommendation.destroy
-    redirect_to shop_path(shop)
   end
 end
