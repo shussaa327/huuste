a158f9856678b52cc56fc3eda9027150233a64d8
Shunsuke <shun.sskd05@icloud.com>
2020-03-07 14:20:49 +0000

    [Fix] お気に入り機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @favorites_posts = Post.where(id: @user.favorites)
+
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2020_03_05_142827) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_favorites_on_user_id", unique: true
   end
 
   create_table "post_comments", force: :cascade do |t|
