5a7d7e536d7d3e68d58a36487221cf51092e7e9b
Shunsuke <shun.sskd05@icloud.com>
2020-03-11 10:26:52 +0000

    [Update] homes/top フォローしているユーザーの投稿を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,9 @@ class User < ApplicationRecord
   def following?(user)
     following_user.include?(user)
   end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
 end
