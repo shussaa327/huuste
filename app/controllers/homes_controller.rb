057d2742b04e565126cbd45cffafeabcc6b5e6a1
Shunsuke <shun.sskd05@icloud.com>
2020-04-15 09:11:34 +0000

    [Update] 検索メソッド,users/index呼び出し, その他細かな修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,11 @@ class HomesController < ApplicationController
   def top
     if user_signed_in?
       @posts = current_user.feed.page(params[:posts_pagina]).order(created_at: :desc)
+      @users = User.where.not(id: current_user.id).page(params[:users_pagina]).reverse_order
     else
       @posts = Post.page(params[:posts_pagina]).order(created_at: :desc)
-    end
       @users = User.page(params[:users_pagina]).reverse_order
+    end
       @areas = Area.all
   end
 
