291fedee651a3f3853ece64cbc0f8e1c03e61c22
Shunsuke <shun.sskd05@icloud.com>
2020-04-15 11:40:03 +0000

    [Remove] guest_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def new_guest
-    user = User.find_by(email: 'guest@guest') do |user|
-      user.password = guest1
-    end
-    sign_in(user)
-    redirect_to root_path
-  end
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
